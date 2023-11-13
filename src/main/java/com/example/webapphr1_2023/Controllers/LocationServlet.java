c91a92003cbbc030d2493f46d8b2e4d7bdbebce0
E.sullca <e.sullca@pucp.edu.pe>
2023-11-12 22:52:26 -0500

    normalidad
    jtyjty

--- src/main/java/com/example/webapphr1_2023/Controllers/LocationServlet.java
+++ src/main/java/com/example/webapphr1_2023/Controllers/LocationServlet.java
@@ -1,6 +1,9 @@
 package com.example.webapphr1_2023.Controllers;
 
-import com.example.webapphr1_2023.Beans.*;
+import com.example.webapphr1_2023.Beans.Department;
+import com.example.webapphr1_2023.Beans.Employee;
+import com.example.webapphr1_2023.Beans.Job;
+import com.example.webapphr1_2023.Beans.Location;
 import com.example.webapphr1_2023.Daos.*;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -32,12 +35,7 @@ public class LocationServlet extends HttpServlet {
                 view = req.getRequestDispatcher("location/list.jsp");
                 view.forward(req, resp);;
                 break;
-            case "formCrear":
-                req.setAttribute("listaPaises",countryDao.obtenerListaPaises());
 
-                view = req.getRequestDispatcher("location/formularioNuevo.jsp");
-                view.forward(req, resp);
-                break;
 
             case "editar":
                 if (req.getParameter("id") != null) {
@@ -53,8 +51,8 @@ public class LocationServlet extends HttpServlet {
                     Location loc = locationDao.obtenerLocation(locationId);
 
                     if (loc != null) {
-                        req.setAttribute("location", loc);
-                        req.setAttribute("listaPaises",countryDao.obtenerListaPaises());
+                        req.setAttribute("lccation", loc);
+                        req.setAttribute("listaTrabajos",countryDao.obtenerListaPaises());
 
                         view = req.getRequestDispatcher("location/formularioEditar.jsp");
                         view.forward(req, resp);
@@ -80,7 +78,7 @@ public class LocationServlet extends HttpServlet {
                     Location loc = locationDao.obtenerLocation(locationId);
 
                     if (loc != null) {
-                        locationDao.borrarLocation(locationId);
+                        locationDao.borrarEmpleado(locationId);
                     }
                 }
 
@@ -91,45 +89,8 @@ public class LocationServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
 
-
-
-        String action = req.getParameter("action") == null ? "lista" : req.getParameter("action");
-
-        LocationDao locationDao = new LocationDao();
-
-        Location location = new Location();
-        location.setStreetAddress(req.getParameter("street_address"));
-        location.setPostalCode(req.getParameter("postal_code"));
-        location.setCity(req.getParameter("city"));
-        location.setStateProvince(req.getParameter("state_province"));
-
-
-        Country country = new Country();
-        country.setCountryId(req.getParameter("country_id"));
-        location.setCountry(country);
-
-
-
-
-
-        switch (action) {
-            case "guardar":
-                int employ = locationDao.searchNextId();
-                location.setLocationId(employ);
-                locationDao.guardarLocation(location);
-
-                resp.sendRedirect("LocationServlet");
-                break;
-            case "actualizar":
-                location.setLocationId(Integer.parseInt(req.getParameter("location_id"))); //no olvidar que para actualizar se debe enviar el ID
-
-                locationDao.actualizarLocation(location);
-
-                resp.sendRedirect("LocationServlet");
-
-                break;
-        }
 
 
     }
--- src/main/java/com/example/webapphr1_2023/Daos/LocationDao.java
+++ src/main/java/com/example/webapphr1_2023/Daos/LocationDao.java
@@ -13,8 +13,10 @@ public class LocationDao extends DaoBase {
 
         ArrayList<Location> listaLocation = new ArrayList<>();
 
-        String sql = "select * from locations l\n"+
+        String sql = "select * from hr.locations l\n"+
                     "inner join countries c on l.country_id = c.country_id";
+
+
 
         try (Connection conn = getConnection();
              Statement stmt = conn.createStatement();
@@ -43,7 +45,7 @@ public class LocationDao extends DaoBase {
         Country country = new Country();
         country.setCountryId(rs.getString("c.country_id"));
         country.setCountryName(rs.getString("country_name"));
-        country.setRegionId(rs.getBigDecimal("region_id"));
+        country.setRegionId(rs.getBigDecimal("region_name"));
         location.setCountry(country);
 
         return location;
@@ -55,7 +57,7 @@ public class LocationDao extends DaoBase {
 
         String sql = "select * from hr.locations l\n"+
                 "inner join countries c on l.country_id = c.country_id\n" +
-                "where l.location_id = ?";
+                "where e.employee_id = ?";
 
         try (Connection conn = getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -76,94 +78,6 @@ public class LocationDao extends DaoBase {
     }
 
 
-    public void guardarLocation(Location location) {
+    
 
-        String sql = "INSERT INTO locations (street_address, postal_code, city, state_province,country_id,location_id) "
-                + "VALUES (?,?, ?, ?, ?, ?)";
-
-        try (Connection conn = getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            setLocationData(location, pstmt);
-            pstmt.setInt(6, location.getLocationId());
-
-            pstmt.executeUpdate();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-    public void actualizarLocation(Location location) {
-
-        String sql = "UPDATE locations "
-                + "SET street_address = ?, "
-                + "postal_code = ?, "
-                + "city = ?, "
-                + "state_province = ?, "
-                + "country_id = ? "
-                + "WHERE location_id = ?";;
-
-        try (Connection conn = getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            System.out.println("hola2");
-            setLocationData(location, pstmt);
-            pstmt.setInt(6, location.getLocationId());
-            System.out.println("hola2");
-            pstmt.executeUpdate();
-            System.out.println("hola3");
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public void borrarLocation(int locationId) {
-
-        try (Connection conn = getConnection();
-             PreparedStatement pstmt = conn.prepareStatement("DELETE FROM locations WHERE location_id = ?")
-
-        ) {
-
-            pstmt.setInt(1, locationId);
-            pstmt.executeUpdate();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-    }
-        private void setLocationData (Location location, PreparedStatement pstmt) throws SQLException {
-
-            pstmt.setString(1, location.getStreetAddress());
-            pstmt.setString(2, location.getPostalCode());
-            pstmt.setString(3, location.getCity());
-            pstmt.setString(4, location.getStateProvince());
-            if ("0".equalsIgnoreCase(location.getCountry().getCountryId())){
-                pstmt.setNull(5,Types.CHAR);
-            }else{
-                pstmt.setString(5, location.getCountry().getCountryId());
-            };
-
-
-
-        }
-    public int searchNextId() {
-        int lastId = 0;
-        String sql = "SELECT MAX(location_id) as last_id FROM locations";
-        try (Connection conn = getConnection();
-             Statement statement  = conn.prepareStatement(sql)) {
-
-            ResultSet resultSet = statement.executeQuery(sql);
-
-            if (resultSet.next()) {
-                lastId = resultSet.getInt("last_id");
-            }
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }
-        System.out.println(lastId);
-        return lastId+100;
-
-    }
-    }
-
+}
